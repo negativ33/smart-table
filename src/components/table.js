5a2853ca3034a44d1634c8662ddcce689029acfe
Dmitrii Vinokurtsev <winokdidi@yandex.ru>
2025-08-04 09:39:07 +0300

    Первйы коммит

--- src/components/table.js
+++ src/components/table.js
@@ -12,12 +12,51 @@ export function initTable(settings, onAction) {
     const root = cloneTemplate(tableTemplate);
 
     // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
+    before.slice().reverse().forEach(subName => {
+        root[subName] = cloneTemplate(subName);
+        root.container.prepend(root[subName].container);
+    })
+
+    after.forEach(subName => {
+        root[subName] = cloneTemplate(subName);
+        root.container.append(root[subName].container)
+    })
 
     // @todo: #1.3 —  обработать события и вызвать onAction()
+    root.container.addEventListener('change', () => {
+        onAction();
+    });
+
+    root.container.addEventListener('reset', () => {
+        setTimeout(onAction)
+    });
+
+    root.container.addEventListener( 'submit', () => {
+        e.preventDefault()
+        onAction(e.submitter)
+    })
+
+
 
     const render = (data) => {
         // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
-        const nextRows = [];
+        const nextRows = data.map(item => {
+            const row = cloneTemplate(rowTemplate)
+
+            Object.keys(item).forEach(key => {
+                if (row.elements[key]) {
+                    const el = row.elements[key]
+                    if (el instanceof HTMLInputElement || el instanceof HTMLSelectElement) {
+                        el.value = item[key];
+                    } else {
+                        el.textContent = item[key]
+                    }
+                }
+            })
+
+            return row.container
+        });
+
         root.elements.rows.replaceChildren(...nextRows);
     }
 
